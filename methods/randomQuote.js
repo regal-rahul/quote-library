ced295e0ea5b3ea46c23713192d96148a7ab5696
regal-rahul <regalrahul777@gmail.com>
2022-08-16 15:32:56 +0530

    removed duplicate data

--- methods/randomQuote.js
+++ methods/randomQuote.js
@@ -1,7 +1,11 @@
-let data = require('../data/quotes.json')
+let data = require('../data/quotes.json');
 
 const randomQuote = () => {
+    const keys = Object.keys(data)
+    const randomIndex = Math.floor(Math.random() * keys.length)
+    const randomKey = keys[randomIndex]
+    const quote = data[randomKey]
+    return quote;
 }
 
 module.exports = randomQuote
-
